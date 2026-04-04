bbed5afc90b20e6ed02db3138926fdcf65a1abee
Qingsong Hu <kingsanhu@gmail.com>
2026-04-04 21:26:43 +0800

    Finish part 1: Fetching Data

--- badger-book/app.js
+++ badger-book/app.js
@@ -1,13 +1,24 @@
 function buildStudents(studs) {
-	// TODO This function is just a suggestion! I would suggest calling it after
-	//      fetching the data or performing a search. It should populate the
-	//      index.html with student data by using createElement and appendChild.
+    // TODO This function is just a suggestion! I would suggest calling it after
+    //      fetching the data or performing a search. It should populate the
+    //      index.html with student data by using createElement and appendChild.
 }
 
 function handleSearch(e) {
-	e?.preventDefault(); // You can ignore this; prevents the default form submission!
+    e?.preventDefault(); // You can ignore this; prevents the default form submission!
 
-	// TODO Implement the search
+    // TODO Implement the search
 }
 
 document.getElementById("search-btn").addEventListener("click", handleSearch);
+
+fetch("https://cs571.org/rest/s25/hw2/students", {
+    method: "GET",
+    headers: {
+        "X-CS571-ID": CS571.getBadgerId(),
+    },
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    });

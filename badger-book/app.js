2ffb78f64c84c58b64ded157c0c9c9345c9078c9
Qingsong Hu <kingsanhu@gmail.com>
2026-04-04 21:58:02 +0800

    Finish part 3: Displaying Students

--- badger-book/app.js
+++ badger-book/app.js
@@ -20,5 +20,37 @@ fetch("https://cs571.org/rest/s25/hw2/students", {
 })
     .then((response) => response.json())
     .then((data) => {
+        // Show number of students
         document.getElementById("num-results").innerText = data.length;
+
+        // Display students
+        const stusNode = document.getElementById("students");
+        for (stu of data) {
+            const nameNode = document.createElement("h2");
+            nameNode.innerText = `${stu.name.first} ${stu.name.last}`;
+            stusNode.appendChild(nameNode);
+
+            const majorNode = document.createElement("p");
+            const emMajorNode = document.createElement("strong");
+            emMajorNode.innerText = stu.major;
+            majorNode.appendChild(emMajorNode);
+            stusNode.appendChild(majorNode);
+
+            const numOfCredsAndHometownNode = document.createElement("p");
+            const isOrNotFromWi = stu.fromWisconsin ? "is" : "is not";
+            numOfCredsAndHometownNode.innerText = `${stu.name.first} is taking ${stu.numCredits} and ${isOrNotFromWi} from Wisconsin.`;
+            stusNode.appendChild(numOfCredsAndHometownNode);
+
+            const interestsNode = document.createElement("p");
+            interestsNode.innerText = `They have ${stu.interests.length} including...`;
+            stusNode.appendChild(interestsNode);
+
+            const interestsListNode = document.createElement("ul");
+            stu.interests.map((interest) => {
+                const itemNode = document.createElement("li");
+                itemNode.innerText = interest;
+                interestsListNode.appendChild(itemNode);
+            });
+            stusNode.appendChild(interestsListNode);
+        }
     });

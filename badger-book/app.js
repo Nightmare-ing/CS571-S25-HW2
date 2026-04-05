a1b13fb738c9cce5cd49dcc386679f0e13abda54
Qingsong Hu <kingsanhu@gmail.com>
2026-04-05 11:31:49 +0800

    Fix bugs of part 5
    - Use given `buildStudents` functions, no need to create a new one
    - Searching interests doesn't support substring searching

--- badger-book/app.js
+++ badger-book/app.js
@@ -1,9 +1,45 @@
 let DATA = undefined;
 
 function buildStudents(studs) {
-    // TODO This function is just a suggestion! I would suggest calling it after
-    //      fetching the data or performing a search. It should populate the
-    //      index.html with student data by using createElement and appendChild.
+    // Show number of students
+    document.getElementById("num-results").innerText = studs.length ?? "???";
+
+    // Display students
+    const stusNode = document.getElementById("students");
+    // clear inner contents
+    stusNode.innerHTML = "";
+    for (stu of studs) {
+        const stuNode = document.createElement("div");
+        stuNode.className = "col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3";
+        const nameNode = document.createElement("h2");
+        nameNode.innerText = `${stu.name.first} ${stu.name.last}`;
+        stuNode.appendChild(nameNode);
+
+        const majorNode = document.createElement("p");
+        const emMajorNode = document.createElement("strong");
+        emMajorNode.innerText = stu.major;
+        majorNode.appendChild(emMajorNode);
+        stuNode.appendChild(majorNode);
+
+        const numOfCredsAndHometownNode = document.createElement("p");
+        const isOrNotFromWi = stu.fromWisconsin ? "is" : "is not";
+        numOfCredsAndHometownNode.innerText = `${stu.name.first} is taking ${stu.numCredits} and ${isOrNotFromWi} from Wisconsin.`;
+        stuNode.appendChild(numOfCredsAndHometownNode);
+
+        const interestsNode = document.createElement("p");
+        interestsNode.innerText = `They have ${stu.interests.length} including...`;
+        stuNode.appendChild(interestsNode);
+
+        const interestsListNode = document.createElement("ul");
+        stu.interests.map((interest) => {
+            const itemNode = document.createElement("li");
+            itemNode.innerText = interest;
+            interestsListNode.appendChild(itemNode);
+        });
+        stuNode.appendChild(interestsListNode);
+
+        stusNode.appendChild(stuNode);
+    }
 }
 
 function handleSearch(e) {
@@ -38,14 +74,15 @@ function handleSearch(e) {
     }
 
     if (searchInterest) {
-        data = data.filter((item) =>
-            item.interests
-                .map((interest) => interest.toLowerCase())
-                .includes(searchInterest),
+        data = data.filter(
+            (item) =>
+                item.interests.filter((interest) =>
+                    interest.toLowerCase().includes(searchInterest),
+                ).length,
         );
     }
 
-    displayData(data);
+    buildStudents(data);
 }
 
 document.getElementById("search-btn").addEventListener("click", handleSearch);
@@ -59,47 +96,5 @@ fetch("https://cs571.org/rest/s25/hw2/students", {
     .then((response) => response.json())
     .then((data) => {
         DATA = data;
-        displayData(data);
+        buildStudents(data);
     });
-
-function displayData(data) {
-    // Show number of students
-    document.getElementById("num-results").innerText = data.length ?? "???";
-
-    // Display students
-    const stusNode = document.getElementById("students");
-    // clear inner contents
-    stusNode.innerHTML = "";
-    for (stu of data) {
-        const stuNode = document.createElement("div");
-        stuNode.className = "col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3";
-        const nameNode = document.createElement("h2");
-        nameNode.innerText = `${stu.name.first} ${stu.name.last}`;
-        stuNode.appendChild(nameNode);
-
-        const majorNode = document.createElement("p");
-        const emMajorNode = document.createElement("strong");
-        emMajorNode.innerText = stu.major;
-        majorNode.appendChild(emMajorNode);
-        stuNode.appendChild(majorNode);
-
-        const numOfCredsAndHometownNode = document.createElement("p");
-        const isOrNotFromWi = stu.fromWisconsin ? "is" : "is not";
-        numOfCredsAndHometownNode.innerText = `${stu.name.first} is taking ${stu.numCredits} and ${isOrNotFromWi} from Wisconsin.`;
-        stuNode.appendChild(numOfCredsAndHometownNode);
-
-        const interestsNode = document.createElement("p");
-        interestsNode.innerText = `They have ${stu.interests.length} including...`;
-        stuNode.appendChild(interestsNode);
-
-        const interestsListNode = document.createElement("ul");
-        stu.interests.map((interest) => {
-            const itemNode = document.createElement("li");
-            itemNode.innerText = interest;
-            interestsListNode.appendChild(itemNode);
-        });
-        stuNode.appendChild(interestsListNode);
-
-        stusNode.appendChild(stuNode);
-    }
-}
